30d3c2bc1e739ef9c98fc2733722cb1764d375e8
Ander Gardenas <anders.gardenas@gmail.com>
2015-10-15 13:08:17 +0200

    fixed some stuf

--- calculate.py
+++ calculate.py
@@ -13,13 +13,14 @@ def calculate(adresses):
     subprocess.call("export LC_ALL=C",shell = True)
     print "Working"
     for adress in adresses:
-       # crul = 'curl -o ' + adress +  ' http://smog.uppmax.uu.se:8080/swift/v1/g6proj/' + address
-       # subprocess.call(curl, shell = True)
-        req = urllib2.Request("http://smog.uppmax.uu.se:8080/swift/v1/g6proj/" + adress)
-        response = urllib2.urlopen(req)
+        print adress
+        crul = 'curl -o ' + adress +  ' http://smog.uppmax.uu.se:8080/swift/v1/g6proj/' + address
+        subprocess.call(curl, shell = True)
+        #req = urllib2.Request("http://smog.uppmax.uu.se:8080/swift/v1/g6proj/" + adress)
+        #response = urllib2.urlopen(req)
         #obj = response.read()
         line = "navier_stokes_solver/airfoil 1 0.0001 10. 0.1 "
-        subprocess.call([line,response], shell = True)
+        subprocess.call(line, shell = True)
         print "done with calculate"
 
 

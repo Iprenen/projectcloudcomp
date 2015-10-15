7c02b443020460063ee9584fc1cd0a818d6d3251
Ander Gardenas <anders.gardenas@gmail.com>
2015-10-15 13:06:33 +0200

    fixed some stuf

--- calculate.py
+++ calculate.py
@@ -18,8 +18,8 @@ def calculate(adresses):
         req = urllib2.Request("http://smog.uppmax.uu.se:8080/swift/v1/g6proj/" + adress)
         response = urllib2.urlopen(req)
         #obj = response.read()
-        line = "navier_stokes_solver/airfoil 1 0.0001 10. 0.1 " + response
-        subprocess.call(line, shell = True)
+        line = "navier_stokes_solver/airfoil 1 0.0001 10. 0.1 "
+        subprocess.call([line,response], shell = True)
         print "done with calculate"
 
 

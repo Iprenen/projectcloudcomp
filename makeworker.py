0773d6b935a1c5098ee77419e6ea39451ccb0a4a
Eric Stenberg <eric.stenberg@telia.com>
2015-10-15 15:56:03 +0200

    tried to fix worker code

---
+++ makeworker.py
@@ -0,0 +1,40 @@
+#Set up enviorment variables needed for autentication
+import os
+
+config = {'username':os.environ['OS_USERNAME'], 
+          'api_key':os.environ['OS_PASSWORD'],
+          'project_id':os.environ['OS_TENANT_NAME'],
+          'auth_url':os.environ['OS_AUTH_URL'],
+           }
+#Set up Nova client
+from novaclient.client import Client
+nc = Client('2',**config)
+
+import time
+ 
+#Create server
+if not nc.keypairs.findall(name="cloudproj"):
+    with open(os.path.expanduser('cloudproj.pub')) as fpubkey:
+        nc.keypairs.create(name="cloudproj", public_key=fpubkey.read())
+image = nc.images.find(name="MOLNS_OpenStack_Provider_1444166469")
+flavor = nc.flavors.find(name="m1.medium")
+user_data = open("userdata.yml", 'r')
+keyname = "cloudproj"
+instance1 = nc.servers.create(name="G6-Project-worker1", image=image, flavor=flavor, key_name=keyname, userdata=user_data)
+instance2 = nc.servers.create(name="G6-Project-worker2", image=image, flavor=flavor, key_name=keyname, userdata=user_data)
+instance3 = nc.servers.create(name="G6-Project-worker3", image=image, flavor=flavor, key_name=keyname, userdata=user_data)
+instance4 = nc.servers.create(name="G6-Project-worker4", image=image, flavor=flavor, key_name=keyname, userdata=user_data)
+ 
+ 
+# Poll at 5 second intervals, until the status is no longer 'BUILD'
+status = instance.status
+while status == 'BUILD':
+    time.sleep(5)
+    # Retrieve the instance again so the status field updates
+    instance = nc.servers.get(instance.id)
+    status = instance.status
+print "status: %s" % status
+
+print "Time out in 90 seconds to give the server a chance to start services and install packages." + "\nWhy don't you get a cup of coffee?"
+time.sleep(90)
+print "Server should be good to go. Please proceed."
---
+++ run.py
@@ -0,0 +1,107 @@
+#!flask/bin/python
+
+from celery import Celery
+from celery import group
+from tasks import wordcount
+from flask import Flask, jsonify
+import subprocess
+import sys
+import os
+import swiftclient.client
+import json
+import time
+import urllib2
+from collections import Counter
+
+import subprocess
+import urkkib2
+response = urllib2.urlopen()
+#file = response.read()
+
+line = "run.sh " + str(response)
+
+subprocess.call(line)
+
+
+toReturn = tweetTask.get()
+
+mesh = []
+req = urllib2.Request("http://smog.uppmax.uu.se:8080/swift/v1/g6proj")
+response = urllib2.urlopen(req)
+meshObject = response.read().split()
+for t in meshObject:
+mesh.append(t)
+
+A = mesh[:4]
+B = mesh[4:8]
+C = mesh[8:12]
+D = mesh[12:16]
+E = mesh[16:]
+
+job = group(calculate.s(A), 
+            calculate.s(B), 
+            calculate.s(C),
+            calculate.s(D),
+            calculate.s(E))
+
+'''
+
+#!flask/bin/python
+
+from celery import Celery
+from celery import group
+from tasks import wordcount
+from flask import Flask, jsonify
+import subprocess
+import sys
+import os
+import swiftclient.client
+import json
+import time
+import urllib2
+from collections import Counter
+
+app = Flask(__name__)
+
+@app.route('/run', methods=['GET'])
+def cow_say():
+	tweetTask = job.apply_async()
+	print "Celery is working..."
+	counter = 0
+	while (tweetTask.ready() == False):
+		print "... %i s" %(counter)
+		counter += 5
+		time.sleep(5)
+	print "The task is done!"
+
+	toReturn = tweetTask.get()
+
+	tweets = []
+	req = urllib2.Request("http://smog.uppmax.uu.se:8080/swift/v1/tweets")
+	response = urllib2.urlopen(req)
+	tweetsObject = response.read().split()
+	for t in tweetsObject:
+		tweets.append(t)
+
+	A = tweets[:4]
+	B = tweets[4:8]
+	C = tweets[8:12]
+	D = tweets[12:16]
+	E = tweets[16:]
+
+	job = group(calculate.s(A), 
+		calculate.s(B), 
+		calculate.s(C),
+		calculate.s(D),
+		calculate.s(E))
+
+	c = Counter()
+	for d in toReturn:
+		c.update(d)
+
+	return jsonify(dict(c)), 200
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0',debug=True)
+
+ '''

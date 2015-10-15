98391181d46726021cd497afcc696d32dc04b443
Ander Gardenas <anders.gardenas@gmail.com>
2015-10-15 10:12:46 +0200

    fixed some stuf

--- calculate.py
+++ calculate.py
@@ -8,13 +8,13 @@ app = Celery('tasks', backend='amqp', broker='amqp://worker:worker@192.168.0.152
 @app.task
 def calculate():
    # for adress in adresses:
-adress = 'r2a9n200.msh.xml'
-req = urllib2.Request("http://smog.uppmax.uu.se:8080/swift/v1/g6proj/" + adress)
-response = urllib2.urlopen(req)
-obj = response.read()
+    adress = 'r2a9n200.msh.xml'
+    req = urllib2.Request("http://smog.uppmax.uu.se:8080/swift/v1/g6proj/" + adress)
+    response = urllib2.urlopen(req)
+    obj = response.read()
 
-line = "sudo navier_stokes_solver/airfoil 1 0.0001 10. 0.1"
-subprocess.call(line, obj)
+    line = "sudo navier_stokes_solver/airfoil 1 0.0001 10. 0.1"
+    subprocess.call(line, obj)
 
 
 

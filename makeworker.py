a0f4b234a0d2386879f5105762349d8c2dac58f8
Eric Stenberg <eric.stenberg@telia.com>
2015-10-20 11:56:45 +0200

    Added return + celery flower

--- makeworker.py
+++ makeworker.py
@@ -1,5 +1,7 @@
 #Set up enviorment variables needed for autentication
 import os
+import time
+import sys
 
 config = {'username':os.environ['OS_USERNAME'], 
           'api_key':os.environ['OS_PASSWORD'],
@@ -10,31 +12,52 @@ config = {'username':os.environ['OS_USERNAME'],
 from novaclient.client import Client
 nc = Client('2',**config)
 
-import time
- 
-#Create server
-if not nc.keypairs.findall(name="cloudproj"):
-    with open(os.path.expanduser('cloudproj.pub')) as fpubkey:
-        nc.keypairs.create(name="cloudproj", public_key=fpubkey.read())
-image = nc.images.find(name="MOLNS_OpenStack_Provider_1444166469")
-flavor = nc.flavors.find(name="m1.medium")
-user_data = open("userdata.yml", 'r')
-keyname = "cloudproj"
-instance1 = nc.servers.create(name="G6-Project-worker1", image=image, flavor=flavor, key_name=keyname, userdata=user_data)
-instance2 = nc.servers.create(name="G6-Project-worker2", image=image, flavor=flavor, key_name=keyname, userdata=user_data)
-instance3 = nc.servers.create(name="G6-Project-worker3", image=image, flavor=flavor, key_name=keyname, userdata=user_data)
-instance4 = nc.servers.create(name="G6-Project-worker4", image=image, flavor=flavor, key_name=keyname, userdata=user_data)
- 
- 
-# Poll at 5 second intervals, until the status is no longer 'BUILD'
-status = instance.status
-while status == 'BUILD':
-    time.sleep(5)
-    # Retrieve the instance again so the status field updates
-    instance = nc.servers.get(instance.id)
-    status = instance.status
-print "status: %s" % status
+workerlist = []
 
-print "Time out in 90 seconds to give the server a chance to start services and install packages." + "\nWhy don't you get a cup of coffee?"
-time.sleep(90)
-print "Server should be good to go. Please proceed."
+def createserver(number, i): 
+    #Create server
+    k = 0
+    while number-(i+k) > 0:
+        k+=1
+        if not nc.keypairs.findall(name="cloudproj"):
+            with open(os.path.expanduser('cloudproj.pub')) as fpubkey:
+                nc.keypairs.create(name="cloudproj", public_key=fpubkey.read())
+        imagew = nc.images.find(name="MOLNS_OpenStack_Provider_1444166469")
+        flavorw = nc.flavors.find(name="m1.medium")
+        user_dataw = open("user_worker.yml", 'r')
+        keynamew = "cloudproj"
+        namew = "G6-Project-worker" + str(i+k)
+        instancew = nc.servers.create(name=namew, image=imagew, flavor=flavorw, key_name=keynamew, userdata=user_dataw)
+        workerlist.append(instancew)
+
+    
+
+ 
+ 
+    # Poll at 5 second intervals, until the status is no longer 'BUILD'
+    for instance in workerlist:
+        status = instance.status
+        while status == 'BUILD':
+            time.sleep(5)
+            # Retrieve the instance again so the status field updates
+            instance = nc.servers.get(instance.id)
+            status = instance.status
+            name = instance.name
+        print "Instance " + str(name) + " have status " + str(status) + "\n"
+
+i = 0
+ser_list = nc.servers.findall()
+for server in ser_list: 
+    if ("G6-Project-worker" in server.name):
+        i+=1
+
+print "There are a total of " + str(i) + " workers."        
+numberofw = raw_input("What is the total number of workers you need? (Included already deployed): ")
+numberofw = int(numberofw)
+if numberofw <= i:
+    print "Aborting, you already have requested number of workers"
+else:
+    createserver(numberofw, i)
+    print "Time out in 90 seconds to give the server(s) a chance to start services and install packages." + "\nWhy don't you get a cup of coffee?"
+    time.sleep(90)
+    print "Server(s) should be good to go. Please proceed."

eb4e511dcc5ac4a980cc6dd20dd49298643de3a9
Anders <anders.gardenas@gmail.com>
2015-10-20 15:58:21 +0200

    new host ip

--- calculate.py
+++ calculate.py
@@ -10,18 +10,15 @@ app = Celery('tasks', backend='amqp', broker='amqp://worker:worker@192.168.0.53/
 @app.task
 def calculate(adresses,args):
     print "started"
-    #subprocess.call("export LC_ALL=C",shell = True)
+ 
     print "started with:"
     for adress in adresses:
         print adress
         curl = "curl -o " + adress +  " http://smog.uppmax.uu.se:8080/swift/v1/g6proj/" + adress
         subprocess.call(curl, shell = True)
-        #req = urllib2.Request("http://smog.uppmax.uu.se:8080/swift/v1/g6proj/" + adress)
-        #response = urllib2.urlopen(req)
-        #obj = response.read()
         line = "navier_stokes_solver/airfoil " + args + " " + adress
         print "line to run " + line 
-        #subprocess.call("export LC_ALL=en_US.UTF-8",shell = True)
+
         subprocess.call(line, shell = True)
         subprocess.call("rm " + adress, shell = True)
         print "done with calculate"
@@ -30,5 +27,5 @@ def calculate(adresses,args):
 
 
 
-        #
+        
     

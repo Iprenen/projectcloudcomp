fce3182875ba067be896626ebeb9b24aae356d18
Anders <anders.gardenas@gmail.com>
2015-10-20 16:36:14 +0200

    o

--- calculate.py
+++ calculate.py
@@ -13,7 +13,6 @@ def calculate(adresses,args):
     subprocess.call("export LC_ALL=C",shell = True)
     print "started with:"
     result = []
-    return 0
     for adress in adresses:
         print adress
         curl = "curl -o " + adress +  " http://smog.uppmax.uu.se:8080/swift/v1/g6proj/" + adress

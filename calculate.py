619f9609a5ccea14a8b6b6f4511f0da65d71057f
Anders <anders.gardenas@gmail.com>
2015-10-20 23:45:20 +0200

    less work for debbugign

--- calculate.py
+++ calculate.py
@@ -10,18 +10,18 @@ app = Celery('tasks', backend='amqp', broker='amqp://worker:worker@192.168.0.53/
 @app.task
 def calculate(adresses,args):
     print "started"
-    subprocess.call("export LC_ALL=C",shell = True)
+    subprocess.call("sudo export LC_ALL=C",shell = True)
     print "started with:"
     result = []
     for adress in adresses:
         print adress
-        curl = "curl -o " + adress +  " http://smog.uppmax.uu.se:8080/swift/v1/g6proj/" + adress
+        curl = "sudo curl -o " + adress +  " http://smog.uppmax.uu.se:8080/swift/v1/g6proj/" + adress
         subprocess.call(curl, shell = True)
-        line = "navier_stokes_solver/airfoil " + args + " " + adress
+        line = "sudo navier_stokes_solver/airfoil " + args + " " + adress
         print "line to run " + line 
         subprocess.call("export LC_ALL=en_US.UTF-8",shell = True)
         subprocess.call(line, shell = True)
-        subprocess.call("rm " + adress, shell = True)
+        subprocess.call("sudo rm " + adress, shell = True)
         print "done with calculate"
 
 

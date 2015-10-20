8354f235c59ef1a21805b09fb7f8aac1e5adc36d
Anders <anders.gardenas@gmail.com>
2015-10-20 15:53:52 +0200

    new host ip

--- calculate.py
+++ calculate.py
@@ -5,7 +5,7 @@ import subprocess
 
 
 
-app = Celery('tasks', backend='amqp', broker='amqp://worker:worker@192.168.0.152/rabbithost')
+app = Celery('tasks', backend='amqp', broker='amqp://worker:worker@192.168.0.53/rabbithost')
 
 @app.task
 def calculate(adresses,args):
